5ad6c44931decd089463c3937d54a7b5d646c257
Catechacha <cate.falchi@tiscali.it>
2017-04-21 14:14:28 +0200

    before lesson 21/04  (fixed some bug)

--- 2017_04_03/main.js
+++ 2017_04_03/main.js
@@ -13,22 +13,27 @@ var xAxis = d3.svg.axis()
     .orient("top");
 
 svg.append("g")
-    .attr("axis")
+    .attr("class","axis")
+    .attr("transform","translate(10,40)")
     .call(xAxis)
+
+var gnumbers = svg.append("g")
+    .attr("class","numbers")
+    .attr("transform","translate(10,60)");
 
 function generateNumbers(){
     var numbers=[];
     var n = Math.round(Math.random()*15); //prendi l'intero più vicino di un numero da 0 a 15. Mi serve per decidere quanti numeri voglio mettere nel dataset
 
     numbers = d3.range(n).map(function(d){
-        return Math.round(Math.random()*250);
+        return Math.round(Math.random()*2500);
     });
+    console.log("numbers", numbers);
     return numbers;
 }
 
 function visualizeNumbers(myNumbers){
-
-    var gs = svg.selectAll("g")
+    var gs = gnumbers.selectAll("g.number")
         .data(myNumbers);
 
     //exit
@@ -38,26 +43,33 @@ function visualizeNumbers(myNumbers){
     var gsToAdd = gs
         .enter()
         .append("g")
+        .attr("class","number")
         .attr("transform",function (d, i){
             return "translate(10,"+(i*14+10)+")";
         });
-    gsToAdd.append("line");
-    gsToAdd.append("text");
+    gsToAdd.append("line").attr("x2",0).attr("stroke","black");
+    gsToAdd.append("text").attr("x",0);
 
     //update
-    var scale = d3.scale.log()
-        .domain([0,2500])
-        .range([0.0001,580]);
+    var scale = d3.scale.linear()
+        .domain([0,d3.max(myNumbers)])
+        .range([0,580]);
+
+    xAxis.scale(scale);
+
+    svg.select("g.axis")
+        .call(xAxis);
 
     var cScale = d3.scale.linear()
-        .domain([0,2500])
-        .range(['#fff','#f00'])
+        .domain([0,d3.max(myNumbers)])
+        .range(["#fff", "#f00"]);
 
     gs.select("line")
         .transition()//x mettere la transizione da uno shuffle ad un altro
         .duration(1500)//x la durata della transizione
         .attr("x2",scale)
         .attr("stroke",cScale);
+
     gs.select("text")
         .transition()//x mettere la transizione da uno shuffle ad un altro
         .duration(1500)//x la durata della transizione
@@ -66,10 +78,11 @@ function visualizeNumbers(myNumbers){
 }
 
 var numbers = generateNumbers();
-console.log('numbers',numbers);
+
 visualizeNumbers(numbers);
 
 d3.select("#shuffle")
-    .on('click',function(){
+    .on('click',function(d){
+        console.log("click");
         visualizeNumbers(generateNumbers())
     });
